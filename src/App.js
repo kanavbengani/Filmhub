994e16974ce566641085d43bf69c11728b9f45a0
Ricky Bengani <rickybengani@gmail.com>
2020-08-08 02:04:43 -0500

    Updated Website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useRef, useEffect } from "react";
+import { useLocation, Switch } from "react-router-dom";
+import AppRoute from "./utils/AppRoute";
+import ScrollReveal from "./utils/ScrollReveal";
+import ReactGA from "react-ga";
 
-import Home from "./components/Home";
+// Layouts
+import LayoutDefault from "./layouts/LayoutDefault";
+
+// Views
+import Home from "./views/Home";
 import signIn from "./components/SignIn";
-import Navigation from "./components/Navigation";
 import load from "./components/Load";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Navigation />
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/signin" component={signIn} />
-            <Route path="/load" component={load} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = (page) => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add("is-loaded");
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  return (
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          <AppRoute path="/signin" component={signIn} />
+          <AppRoute path="/load" component={load} />
+        </Switch>
+      )}
+    />
+  );
+};
 
 export default App;
